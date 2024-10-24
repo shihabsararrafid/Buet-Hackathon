84cd780867e49f2262bb3e0a8de98ee5f21d8491
shihabsararrafid <shrafid.532@gmail.com>
2024-10-25 05:26:24 +0600

    feat:updated

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,14 +1,24 @@
 // app.js
-const express = require('express');
-const config = require('./config/index');
-const authController = require('./controllers/auth');
-const userRouter  = require('./routes/userRoutes');
-const { createUser } = require('./controllers/userController');
+const express = require("express");
+const config = require("./config/index");
+const authController = require("./controllers/auth");
+const userRouter = require("./routes/userRoutes");
+const { createUser } = require("./controllers/userController");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.set('jwtSecretKey', config.jwtSecretKey);
-app.set('tokenHeaderKey', config.tokenHeaderKey);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5173",
+    ],
+  })
+);
+app.set("jwtSecretKey", config.jwtSecretKey);
+app.set("tokenHeaderKey", config.tokenHeaderKey);
 
 // app.post("/user/register",createUser );
 
--- auth-service/server.js
+++ auth-service/server.js
@@ -1,39 +1,21 @@
-// server.js
-// import "dotenv/config";
-// import cookieParser from "cookie-parser";
-// import express from "express";
-
-// const app = express();
-// const PORT = process.env.PORT || 4000;
-
-// // * Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// app.get("/", (req, res) => {
-//   return res.send("Hi Everyone. ");
-// });
-
-// // * routes file
-// import userRouter from "./routes/userRoutes.js"
-// import postRouter from "./routes/postRoutes.js"
-// import commentRouter from "./routes/commentRoutes.js"
-
-// app.use("/api/user", userRouter);
-
-// app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-// server.js
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const express = require('express');
-
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // * Middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
